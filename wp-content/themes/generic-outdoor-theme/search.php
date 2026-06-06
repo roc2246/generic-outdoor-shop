deccb6f37ec28b88ac29cd3b679a89e21ac40efd
Riley <riley.childs@yahoo.com>
2026-06-06 13:19:13 -0400

    Included line breaks in search results

--- wp-content/themes/generic-outdoor-theme/search.php
+++ wp-content/themes/generic-outdoor-theme/search.php
@@ -3,25 +3,26 @@
 get_header();
 pageBanner(array(
   'title' => 'Search Results',
-  'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false))  . '&rdquo;'
+  'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false)) . '&rdquo;'
 ));
- ?>
+?>
 
 <div class="container container--narrow page-section">
-<?php
+  <?php
+  get_search_form();
+
+
   if (have_posts()) {
-    while(have_posts()) {
+    echo '<br>';
+    while (have_posts()) {
       the_post();
       get_template_part('template-parts/content', get_post_type());
     }
     echo paginate_links();
   } else {
-    echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
+    echo '<br><h2 class="headline headline--small-plus">No results match that search.</h2>';
   }
-
-  get_search_form();
-  
-?>
+  ?>
 
 </div>
 
